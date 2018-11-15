0b8d187a68fbdaf8353aa3e1d5b0584ecfdcf2d2
klishin16 <klishin.nd16@gmail.com>
2018-11-15 20:38:20 +0300

    Изменена функция run()

--- homework03/life.py
+++ homework03/life.py
@@ -38,20 +38,18 @@ class GameOfLife:
         pygame.display.set_caption('Game of Life')
         self.screen.fill(pygame.Color('white'))
 
-        # Создание списка клеток
-        # PUT YOUR CODE HERE
+        self.cell_list(randomize=True)
 
         running = True
         while running:
             for event in pygame.event.get():
                 if event.type == QUIT:
                     running = False
+
+            self.draw_cell_list(self.clist)
             self.draw_grid()
-
-            # Отрисовка списка клеток
-            # Выполнение одного шага игры (обновление состояния ячеек)
-            # PUT YOUR CODE HERE
-
+            self.update_cell_list(self.clist)
+            
             pygame.display.flip()
             clock.tick(self.speed)
         pygame.quit()
