34fe0ad553d4d39385786f2b185321e508de6bb4
klishin16 <klishin.nd16@gmail.com>
2018-11-29 15:47:25 +0300

    Добавлен файл messages.py

---
+++ homework04/messages.py
@@ -0,0 +1,56 @@
+from collections import Counter
+import datetime
+import plotly
+import plotly.plotly as py
+import plotly.graph_objs as go
+from typing import List, Tuple
+
+from api import messages_get_history
+from api_models import Message
+import config
+
+
+Dates = List[datetime.date]
+Frequencies = List[int]
+
+
+plotly.tools.set_credentials_file(
+    username=config.PLOTLY_CONFIG['username'],
+    api_key=config.PLOTLY_CONFIG['api_key']
+)
+
+
+def fromtimestamp(ts: int) -> datetime.date:
+    return datetime.datetime.fromtimestamp(ts).date()
+
+
+def count_dates_from_messages(messages: List[Message]) -> Tuple[Dates, Frequencies]:
+    """ Получить список дат и их частот
+    :param messages: список сообщений
+    """
+    dates = []
+    frequencies = []
+    for message in messages:
+        for i in range(len(dates)):
+            if fromtimestamp(message.date) == dates[i]:
+                frequencies[i] += 1
+                break
+        else:
+            dates.append(fromtimestamp(message.date))
+            frequencies.append(1)
+    return (dates, frequencies)
+
+
+def plotly_messages_freq(dates: Dates, freq: Frequencies) -> None:
+    """ Построение графика с помощью Plot.ly
+    :param date: список дат
+    :param freq: число сообщений в соответствующую дату
+    """
+
+    data = [go.Scatter(x=dates, y=freq)]
+    py.plot(data)
+
+
+if __name__ == '__main__':
+    dates, frequencies = count_dates_from_messages(messages_get_history(14445552))
+    plotly_messages_freq(dates, frequencies)
