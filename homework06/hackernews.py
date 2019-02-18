573fa14d435755428688a164b5d54f5ff64198f8
klishin16 <klishin.nd16@gmail.com>
2019-02-18 17:52:12 +0300

    Добавлен файл hackernews.py

---
+++ homework06/hackernews.py
@@ -0,0 +1,72 @@
+from bottle import (
+    route, run, template, request, redirect
+)
+
+from scraputils import get_news
+from db import session, News
+from sqlalchemy import and_
+import string
+from bayes import NaiveBayesClassifier
+
+
+@route("/news")
+def news_list():
+    s = session()
+    rows = s.query(News).filter(News.label is None).all()
+    return template('news_template', rows=rows)
+
+
+@route("/add_label/")
+def add_label():
+    s = session()
+    new = s.query(News).filter(News.id == request.query.id).one()
+    new.label = request.query.label
+    s.commit()
+    redirect("/news")
+
+
+@route("/update")
+def update_news():
+    s = session()
+    latest_news = get_news("https://news.ycombinator.com/newest", n_pages=15)
+    titles = [new['title'] for new in latest_news]
+    authors = [new['author'] for new in latest_news]
+    existing_news = s.query(News).filter(and_((News.author.in_(authors)), (News.title.in_(titles)))).all()
+    print(len(existing_news))
+    for item in latest_news:
+        if item['title'] not in [n.title for n in existing_news] and item['author'] not in [n.author for n in existing_news]:
+            s.add(News(**item))
+    s.commit()
+    redirect("/news")
+
+
+def clean(s):
+    translator = str.maketrans("", "", string.punctuation)
+    return s.translate(translator)
+
+
+@route("/classify")
+def classify_news():
+    s = session()
+    labeled_news = s.query(News).filter(News.label is not None).all()
+    x = [clean(new.title) for new in labeled_news]
+    Y = [new.label for new in labeled_news]
+    model = NaiveBayesClassifier()
+    model.fit(x, y)
+    unlabeled_news = s.query(News).filter(News.label is None).all()
+    good = []
+    maybe = []
+    never = []
+    for new in unlabeled_news:
+        [prediction] = model.predict([clean(new.title)])
+        if prediction == 'good':
+            good.append(new)
+        elif prediction == 'maybe':
+            maybe.append(new)
+        else:
+            never.append(new)
+    return template('recommendations', good=good, maybe=maybe, never=never)
+
+
+if __name__ == "__main__":
+    run(host="localhost", port=8080)
